af38d2e3d1e9dfe73b9e492e367348402bfa2d46
Sherry Martin <sm674@cornell.edu>
2018-09-12 22:09:23 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,6 @@
  #.map would be more appropriate; however, this lesson called # for using .each method
  
- def square_array(array)  
+def square_array(array)  
   array.each_with_object([]) { |i,arr| arr << i ** 2 }
 end
 
